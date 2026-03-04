09e146f1f48fe3152b48a1a7bcfc625110de4b34
Jiwon Moon <ziwonmoon@icloud.com>
2026-03-04 15:59:15 +0900

    baseurl update

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ziwonmoon.github.io/ai-knowledge-web",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
